453a782a718f14e26ccdc8b4cdbe779f78648bde
ericlee215 <51139205+ericlee215@users.noreply.github.com>
2019-12-14 15:07:10 -0700

    Add files via upload

--- untitled0.py
+++ untitled0.py
@@ -4,32 +4,32 @@ import numpy as np
 from Jensen_Model_3D import Jensen_Calc
 from Jensen_Model_3D2 import Jensen_Calc2
 
-u = 8.1
-rnot = 20
-alpha = 0.1
-theta = np.linspace(-20,20,41)
-downwind = np.linspace(120, 320, 41)
-
-
-Model = Jensen_Calc(alpha,downwind,rnot,u,theta)
-Model2 = Jensen_Calc2(alpha,downwind,rnot,u,theta)
-Cos6 = pd.read_excel (r'C:\Users\Owner\Documents\495R\Digitizer Data.xlsx', 'Cos 6') #(use "r" before the path string to address special character, such as '\'). Don't forget to put the file name at the end of the path + '.xlsx'
-Cos6 = np.asarray(Cos6)
-
-Cos10 = pd.read_excel (r'C:\Users\Owner\Documents\495R\Digitizer Data.xlsx', 'Cos 10') #(use "r" before the path string to address special character, such as '\'). Don't forget to put the file name at the end of the path + '.xlsx'
-Cos10 = np.asarray(Cos10)
-
-Cos16 = pd.read_excel (r'C:\Users\Owner\Documents\495R\Digitizer Data.xlsx', 'Cos 16') #(use "r" before the path string to address special character, such as '\'). Don't forget to put the file name at the end of the path + '.xlsx'
-Cos16 = np.asarray(Cos16)
-
-A6 = pd.read_excel (r'C:\Users\Owner\Documents\495R\Digitizer Data.xlsx', 'TH 6') #(use "r" before the path string to address special character, such as '\'). Don't forget to put the file name at the end of the path + '.xlsx'
-A6 = np.asarray(A6)
-
-TH10 = pd.read_excel (r'C:\Users\Owner\Documents\495R\Digitizer Data.xlsx', 'TH 10') #(use "r" before the path string to address special character, such as '\'). Don't forget to put the file name at the end of the path + '.xlsx'
-TH10 = np.asarray(TH10)
-
-TH16 = pd.read_excel (r'C:\Users\Owner\Documents\495R\Digitizer Data.xlsx', 'TH 16') #(use "r" before the path string to address special character, such as '\'). Don't forget to put the file name at the end of the path + '.xlsx'
-TH16 = np.asarray(TH16)
+#u = 8.1
+#rnot = 20
+#alpha = 0.1
+#theta = np.linspace(-20,20,101)
+#downwind = np.linspace(120, 320, 101)
+#
+#
+#Model = Jensen_Calc(alpha,downwind,rnot,u,theta)
+#Model2 = Jensen_Calc2(alpha,downwind,rnot,u,theta)
+#Cos6 = pd.read_excel (r'C:\Users\Owner\Documents\495R\Digitizer Data.xlsx', 'Cos 6') #(use "r" before the path string to address special character, such as '\'). Don't forget to put the file name at the end of the path + '.xlsx'
+#Cos6 = np.asarray(Cos6)
+#
+#Cos10 = pd.read_excel (r'C:\Users\Owner\Documents\495R\Digitizer Data.xlsx', 'Cos 10') #(use "r" before the path string to address special character, such as '\'). Don't forget to put the file name at the end of the path + '.xlsx'
+#Cos10 = np.asarray(Cos10)
+#
+#Cos16 = pd.read_excel (r'C:\Users\Owner\Documents\495R\Digitizer Data.xlsx', 'Cos 16') #(use "r" before the path string to address special character, such as '\'). Don't forget to put the file name at the end of the path + '.xlsx'
+#Cos16 = np.asarray(Cos16)
+#
+#A6 = pd.read_excel (r'C:\Users\Owner\Documents\495R\Digitizer Data.xlsx', 'TH 6') #(use "r" before the path string to address special character, such as '\'). Don't forget to put the file name at the end of the path + '.xlsx'
+#A6 = np.asarray(A6)
+#
+#TH10 = pd.read_excel (r'C:\Users\Owner\Documents\495R\Digitizer Data.xlsx', 'TH 10') #(use "r" before the path string to address special character, such as '\'). Don't forget to put the file name at the end of the path + '.xlsx'
+#TH10 = np.asarray(TH10)
+#
+#TH16 = pd.read_excel (r'C:\Users\Owner\Documents\495R\Digitizer Data.xlsx', 'TH 16') #(use "r" before the path string to address special character, such as '\'). Don't forget to put the file name at the end of the path + '.xlsx'
+#TH16 = np.asarray(TH16)
 
 
 
@@ -42,7 +42,7 @@ fig.tight_layout()
 ax1 = plt.subplot(321)
 plt.title("Top Hat at x/r0 = 6")
 plt.plot(A6[:,0],A6[:,1])
-plt.plot(theta,Model2[0,:,20]/u)
+plt.plot(theta,Model2[0,:,50]/u)
 plt.xlabel('\u0394 \u03F4 Degrees')
 plt.ylabel('u/v')
 plt.setp(ax1.get_xticklabels(), visible=False)
@@ -50,7 +50,7 @@ plt.setp(ax1.get_xticklabels(), visible=False)
 ax2 = plt.subplot(323, sharex = ax1, sharey = ax1)
 plt.title("Top Hat at x/r0 = 10")
 plt.plot(TH10[:,0],TH10[:,1])
-plt.plot(theta,Model2[16,:,20]/u)
+plt.plot(theta,Model2[40,:,50]/u)
 plt.xlabel('\u0394 \u03F4 Degrees')
 plt.ylabel('u/v')
 plt.setp(ax2.get_xticklabels(), visible=False)
@@ -58,7 +58,7 @@ plt.setp(ax2.get_xticklabels(), visible=False)
 ax3 = plt.subplot(325, sharex = ax1, sharey = ax1)
 plt.title("Top Hat at x/r0 = 16")
 plt.plot(TH16[:,0],TH16[:,1])
-plt.plot(theta,Model2[40,:,20]/u)
+plt.plot(theta,Model2[100,:,50]/u)
 plt.xlabel('\u0394 \u03F4 Degrees')
 plt.ylabel('u/v')
 
@@ -66,7 +66,7 @@ plt.ylabel('u/v')
 ax4 = plt.subplot(322, sharex = ax1, sharey = ax1)
 plt.title("Cosine at x/r0 = 6")
 plt.plot(Cos6[:,0],Cos6[:,1])
-plt.plot(theta,Model[0,:,20]/u)
+plt.plot(theta,Model[0,:,50]/u)
 plt.xlabel('\u0394 \u03F4 Degrees')
 plt.ylabel('u/v')
 plt.setp(ax4.get_xticklabels(), visible=False)
@@ -74,7 +74,7 @@ plt.setp(ax4.get_xticklabels(), visible=False)
 ax5 = plt.subplot(324, sharex = ax1, sharey = ax1)
 plt.title("Cosine at x/r0 = 10")
 plt.plot(Cos10[:,0], Cos10[:,1])
-plt.plot(theta,Model[16,:,20]/u)
+plt.plot(theta,Model[40,:,50]/u)
 plt.xlabel('\u0394 \u03F4 Degrees')
 plt.ylabel('u/v')
 plt.setp(ax5.get_xticklabels(), visible=False)
@@ -82,11 +82,11 @@ plt.setp(ax5.get_xticklabels(), visible=False)
 ax6 = plt.subplot(326, sharex = ax1, sharey = ax1)
 plt.title("Cosine at x/r0 = 16")
 plt.plot(Cos16[:,0],Cos16[:,1])
-plt.plot(theta,Model[40,:,20]/u)
+plt.plot(theta,Model[100,:,50]/u)
 plt.xlabel('\u0394 \u03F4 Degrees')
 plt.ylabel('u/v')
 plt.setp(ax1.get_xticklabels(), visible=False)
-plt.legend({"Jensen Paper", "My Model"}, frameon = False, bbox_to_anchor=(-1.1,-.7))
+ax6.legend({"Jensen","My Code"}, frameon = False, bbox_to_anchor=(-1.1,-.7))
 
 fig.savefig('full_figure.png')
 
